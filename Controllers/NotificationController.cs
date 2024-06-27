a8a9ea9bc188f7b9e1ac76f248eade1f34a7a453
IT-Cia <a.neustroeva@outlook.com>
2024-06-27 19:01:07 +0300

    Добавьте файлы проекта.

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,57 @@
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using omnitep_bfb_ms.Services;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace omnitep_bfb_ms.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [SwaggerTag("SwaggerTag on class")]
+    public class NotificationController : ControllerBase
+    {
+        private GrpcService grpc;
+        private IConfiguration configuration;
+        private Notifications.notifications.notificationsClient client;
+
+        public NotificationController(GrpcService grpc, IConfiguration configuration, Notifications.notifications.notificationsClient client)
+        {
+            this.grpc = grpc;
+            this.configuration = configuration;
+            this.client = client;
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<ActionResult<Notifications.SendResponse>> Post([Required] Notifications.SendRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.From))
+                return new BadRequestObjectResult("Parameter 'from' is required");
+            if (string.IsNullOrWhiteSpace(request.To))
+                return new BadRequestObjectResult("Parameter 'to' is required");
+            if (string.IsNullOrWhiteSpace(request.Channel))
+                return new BadRequestObjectResult("Parameter 'channel' is required");
+            if (string.IsNullOrWhiteSpace(request.Message))
+                return new BadRequestObjectResult("Parameter 'message' is required");
+
+            //try
+            //{
+            
+            //var channel = GrpcChannel.ForAddress(configuration.GetValue<string>("GrpcUrl"));
+            //var client = new Notifications.notifications.notificationsClient(channel);
+            var response = await client.SendAsync(request);
+            return new OkObjectResult(response);
+            //}
+            //catch (RpcException ex)
+            //{
+            //    return new BadRequestObjectResult(ex.Status.StatusCode + ": " + ex.Status.Detail);
+            //}
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using omnitep_bfb_ms.Services;
+using System.Configuration;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel(options =>
+{
+    options.ConfigureHttpsDefaults(o =>
+    {
+        //o.ClientCertificateValidation += ValidateClientCertificate;
+        o.AllowAnyClientCertificate();
+        o.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+        o.CheckCertificateRevocation = false;
+    });
+    options.Listen(IPAddress.Loopback, builder.Configuration.GetValue<int>("PortNumber"));
+    options.Listen(IPAddress.Loopback, builder.Configuration.GetValue<int>("PortNumberSSL"),
+    listenOptions =>
+    {
+         listenOptions.UseHttps("omnitep.pfx", "omnitep-bfb-ms-pwd");
+    });
+});
+
+var httpHandler = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+
+builder.Services.AddGrpcClient<Notifications.notifications.notificationsClient>(o =>
+{
+    o.Address = new Uri(builder.Configuration.GetValue<string>("GrpcUrl"));
+}).ConfigureChannel(o =>
+{
+    o.HttpHandler = httpHandler;
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<GrpcService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
